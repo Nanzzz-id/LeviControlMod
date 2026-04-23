900f20acbf2c6fef5e90916954aca6bf38ca4908
Nanzzz-id <luxxstill@gmail.com>
2026-04-23 14:09:52 +0700

    Update Gloss.h

--- include/Gloss.h
+++ include/Gloss.h
@@ -1,1 +1,19 @@
 
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void*     GlossOpen(const char* lib_name);
+void*     GlossSymbol(void* handle, const char* sym_name);
+int       GlossHook(void* target, void* hook, void** orig);
+int       GlossHookByName(void* handle, const char* sym, void* hook, void** orig);
+uintptr_t GlossGetLibBias(void* handle);
+void      GlossInit();
+void      GlossClose(void* handle);
+
+#ifdef __cplusplus
+}
+#endif
